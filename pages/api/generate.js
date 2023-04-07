e469e17ee5f0083946ef730acb2e95d2b72cc2c9
Chirag Rao <73184157+SpaceFace02@users.noreply.github.com>
2023-04-07 22:43:39 +0530

    Convai Challenge

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,29 +10,30 @@ export default async function (req, res) {
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
+  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
-      }
+      },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    // Use chat completion modl, gpt-3.5-turbo model, and 1 prompt, hardcoded.
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0.7,
+      messages: [{ role: "user", content: animal }],
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    res.status(200).json({ result: completion.data.choices[0].message.content });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,16 +42,16 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  console.log(capitalizedAnimal);
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,230 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./index.module.css";
+import prompts from "../prompts";
+import toast, { Toaster } from "react-hot-toast";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BounceLoader from "react-spinners/BounceLoader";
+
+import Modal from "react-modal";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [finalCheck, setFinalCheck] = useState("");
 
   async function onSubmit(event) {
+    setLearnLoading(true);
     event.preventDefault();
+    setIsOpen(true);
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+      const data = await callBE(`Tell me more about ${prompts[currentQuestion].name}`);
 
-      const data = await response.json();
-      if (response.status !== 200) {
+      if (!data) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
+      setLearnLoading(false);
       setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
 
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    marginTop: "5px",
+    marginBottom: "5px",
+  };
+
+  const customStyles = {
+    overlay: { backgroundColor: "rgba(0, 0, 0, 0.6)" },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#F9A826",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    padding: "10px 20px",
+    cursor: "pointer",
+    marginTop: "20px",
+  };
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [quesIdx, setQuesIdx] = useState(0);
+  const [lock, setLock] = useState(0);
+  const [score, setScore] = useState(0);
+  const [fetchedQuestions, setFetchedQuestions] = useState([]);
+  const [learnLoading, setLearnLoading] = useState(false);
+  const [resultLoading, setResultLoading] = useState(false);
+  const [questionsLoading, setQuestionsLoading] = useState(false);
+
+  const handlePrevious = () => {
+    // const prevQues = currentQuestion - 1;
+    // prevQues >= 0 && setCurrentQuestion(prevQues);
+    const prevQues = quesIdx - 1;
+    prevQues >= 0 && setQuesIdx(prevQues);
+  };
+
+  const handleNext = () => {
+    // const nextQues = currentQuestion + 1;
+    //  nextQues < prompts.length && setCurrentQuestion(nextQues);
+    const nextQues = quesIdx + 1;
+    nextQues < prompts.length && setQuesIdx(nextQues);
+    if (score >= (currentQuestion + 1) * 3 && lock === 0) {
+      setLock(currentQuestion + 1);
+      toast.success("You have unlocked the next level!");
+    } else if (quesIdx + 1 == 3 && score < (currentQuestion + 1) * 3 && lock === 0) {
+      toast.error("You have not unlocked the next level yet!");
+      setQuesIdx(2);
+    }
+  };
+
+  const callBE = async (prompt) => {
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        animal: prompt,
+      }),
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const checkAnswer = async () => {
+    setResultLoading(true);
+    const data = await callBE(
+      `Question is '${fetchedQuestions[quesIdx]}', and answer is '${animalInput}', am I correct? Answer in just yes or no`
+    );
+
+    // Only first 3 letters of finalCheck
+    setFinalCheck(data.result);
+    setAnimalInput("");
+    setResultLoading(false);
+    if (data.result === "Yes.") {
+      toast.success(`You have answered ${quesIdx + 1} / 3 questions correctly!`);
+      setScore(score + 1);
+      setQuesIdx(quesIdx + 1);
+      console.log(score);
+      if (score >= (currentQuestion + 1) * 3 - 1) {
+        toast.success("You have unlocked the next level!");
+        setCurrentQuestion(currentQuestion + 1);
+        setQuesIdx(0);
+      }
+    } else {
+      toast.error(
+        "Incorrect answer! Please learn more about the topic by clicking on the button and try again!"
+      );
+    }
+  };
+
+  const getQuestions = async () => {
+    const data = await callBE(
+      `Give me 3 questions regarding '${prompts[currentQuestion].prompt}', sepearated by a line, dont give the answers.`
+    );
+    // data has 3 questions and 3 answers, each seperated by a tilde, extract questions in one array and answers in another.
+    // Map through data, and for each element, split by tilde, and push to questions array and the remaining to answers array.
+    const questions = data.result.split("\n");
+    // remove any empty strings in questions array
+    const filteredQuestions = questions.filter((question) => question !== "");
+    setFetchedQuestions(filteredQuestions);
+    setQuestionsLoading(false);
+  };
+
+  // Whenever current question changes, call the api in generate.js
+  // and set the result to the result of the api call.
+  useEffect(() => {
+    setQuestionsLoading(true);
+    getQuestions();
+  }, [currentQuestion]);
+
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Convai Challenge</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,300;1,400&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <Toaster />
+        <Row style={{ width: "100%", marginBottom: "5px", marginTop: "-10px" }}>
+          <Col>
+            <ProgressBar striped variant="info" now={(currentQuestion / 5) * 100} />
+          </Col>
+        </Row>
+        <Row style={{ width: "80%" }}>
+          <Col>
+            <ProgressBar striped variant="info" now={(quesIdx / 3) * 100} />
+          </Col>
+        </Row>
+        <h3>{prompts[currentQuestion].name}</h3>
+        {questionsLoading ? (
+          <BounceLoader color="#36d7b7" cssOverride={override} size={30} />
+        ) : (
+          <h4>{fetchedQuestions[quesIdx]}</h4>
+        )}
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Intro to ML"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Learn about this topic." />
         </form>
-        <div className={styles.result}>{result}</div>
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+          <h1>Modal Content</h1>
+          {learnLoading ? (
+            <div>
+              <BounceLoader color="#36d7b7" cssOverride={override} size={30} />
+              <h6>OpenAI ChatGPT API is slow, please wait a few seconds.</h6>
+            </div>
+          ) : (
+            <p className={styles.result}>{result}</p>
+          )}
+          <button onClick={() => setIsOpen(false)} style={buttonStyle}>
+            Close Modal
+          </button>
+        </Modal>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 10 }}>
+          <button style={buttonStyle} onClick={handlePrevious}>
+            Previous
+          </button>
+          <button style={buttonStyle} onClick={handleNext}>
+            Next
+          </button>
+        </div>
+        <button style={buttonStyle} onClick={checkAnswer}>
+          Answer!
+        </button>
+        {resultLoading ? (
+          <BounceLoader color="#36d7b7" cssOverride={override} size={30} />
+        ) : (
+          <div className={styles.result}>{finalCheck}</div>
+        )}
       </main>
     </div>
   );
