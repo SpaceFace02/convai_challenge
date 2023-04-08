62e5113bc3bcf4621d1c8ee6de52b7a43aa23c60
Chirag Rao <73184157+SpaceFace02@users.noreply.github.com>
2023-04-08 10:36:34 +0530

    Optimizations

--- pages/index.js
+++ pages/index.js
@@ -121,6 +121,35 @@ export default function Home() {
     setQuestionsLoading(false);
   };
 
+  export const customStyles = {
+    overlay: { backgroundColor: "rgba(0, 0, 0, 0.6)" },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  export const buttonStyle = {
+    backgroundColor: "#F9A826",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    padding: "10px 20px",
+    cursor: "pointer",
+    marginTop: "20px",
+  };
+
+  export const override = {
+    display: "block",
+    margin: "0 auto",
+    marginTop: "5px",
+    marginBottom: "5px",
+  };
+
   // Whenever current question changes, call the api in generate.js
   // and set the result to the result of the api call.
   useEffect(() => {
